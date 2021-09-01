bbe40299cb75577c858bef34cf8b4bdc32a2ece6
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-01 12:14:42 +0530

    ADDED: Welocome Message

---
+++ src/com/bridgelabz/myhashmap/MyHashMapMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.myhashmap;
+
+public class MyHashMapMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("----- Welcome To Data Structure: HashMap -----");
+	}
+
+}
